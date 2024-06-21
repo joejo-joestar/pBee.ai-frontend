863bf9435c6761a4f104c73ea1de74d3edd77e35
Joe <joecn2704+github@gmail.com>
2024-06-21 13:36:45 +0400

    about us pages template

--- src/pages/MoreAboutUs/index.tsx
+++ src/pages/MoreAboutUs/index.tsx
@@ -1,3 +1,5 @@
+import MainHeaderText from "@/components/shared/MainHText";
+
 type Props = {};
 
 const MoreAboutUs = (props: Props) => {
@@ -6,7 +8,47 @@ const MoreAboutUs = (props: Props) => {
       id="moreaboutus"
       className="h-device place-content-center gap-16 bg-gradient-to-b from-[#1c2336] to-[#12131c] py-10"
     >
-      <div>MoreAboutUs</div>
+      <div>
+        <MainHeaderText>About Us</MainHeaderText>
+        <p>
+          Our goal is to make powerful marketing tools accessible and easy to
+          use for businesses.
+        </p>
+      </div>
+      <div>
+        <h1 className="text-3xl font-bold">
+          Simplifying technology to the world by providing a one-stop digital
+          ecosystem
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+      <div>
+        <h1 className="text-3xl font-bold">Our Team </h1>
+        <div className="Frame93 bg-indigo-700/opacity-20 inline-flex h-[385px] w-[287px] flex-col items-center justify-start gap-[42px] rounded-xl border border-indigo-700 p-8">
+          {/* Profile */}
+          <img
+            className="size-64 rounded-full border border-neutral-700"
+            src="https://via.placeholder.com/256x256"
+          />
+          {/* Label */}
+          <div className="flex flex-col items-center justify-center gap-4">
+            {/* Name */}
+            <h3 className="text-center text-xl font-semibold">
+              James Fredrich
+            </h3>
+            {/* Designation */}
+            <p className="text-center font-normal">CEO of xyz inc</p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
